d31a8b3082a01e19e2ac6c886c654d4e8e211320
fritzg06 <35794702+fritzg06@users.noreply.github.com>
2018-04-09 11:58:14 +0800

    Update 04a_CreateVmFromTemplate_WindowsServer201664bitNoTools.ps1

--- 04a_CreateVmFromTemplate_WindowsServer201664bitNoTools.ps1
+++ 04a_CreateVmFromTemplate_WindowsServer201664bitNoTools.ps1
@@ -8,24 +8,24 @@ while ($i -le $NumberToDeploy)
 	{
 	$templateName = "Template Windows Server 2016 64bit notools" #CHANGEME-Source Template
 	$esxName = "cnode-nec01.tekchallenge.local" #CHANGME-Destination Host
-  $rpName = "Production" #CHANGEME-Destination Resource Pool
+  	$rpName = "Production" #CHANGEME-Destination Resource Pool
 	$dsName = "STR-SAS01" #CHANGEME-Destination Datastore
-  $dsCluster = "Support Services Storage" #CHANGEME-Destination Datastore Cluster
+  	$dsCluster = "Support Services Storage" #CHANGEME-Destination Datastore Cluster
 	
-  $VMname = $NamingConvention + $i
+  	$VMname = $NamingConvention + $i
 	$template = Get-Template -Name $templateName
 	
-  # USE COMMENT TO SELECT WHETHER PRE-DEFINED DATASTORE ABOVE ($dsName) OR USE RANDOM DATASTORE FROM THE CLUSTER ($dsCluster)
-  #$ds = Get-Datastore -Name $dsName
+  	# USE COMMENT TO SELECT WHETHER PRE-DEFINED DATASTORE ABOVE ($dsName) OR USE RANDOM DATASTORE FROM THE CLUSTER ($dsCluster)
+  	#$ds = Get-Datastore -Name $dsName
 	$ds = Get-DatastoreCluster -Name $dsCluster | Get-Datastore | Get-Random
 	
-  $esx = Get-VMHost -Name $esxName
+  	$esx = Get-VMHost -Name $esxName
 	$rp = Get-ResourcePool -Name $rpName
 	
-  # NETWORK / IGNORE THIS FOR NOW-IT DOES NOT WORK YET
-  #$network = Get-VirtualPortGroup -name "dv_default"
+  	# NETWORK / IGNORE THIS FOR NOW-IT DOES NOT WORK YET
+  	#$network = Get-VirtualPortGroup -name "dv_default"
 
-  # CHECK IF THE VM EXISTS PRIOR CREATING FROM TEMPLATE
+  	# CHECK IF THE VM EXISTS PRIOR CREATING FROM TEMPLATE
 	if ((Get-VM $VMName -ErrorAction SilentlyContinue).Name -eq $VMName) 
 		{
 		Write-Host "$VMName already exists, skipping creation of this VM!" -ForegroundColor Yellow
@@ -34,11 +34,11 @@ while ($i -le $NumberToDeploy)
 	else
 		{
 		# USE COMMENT WHETHER TO USE HOST OR RESOURCE POOL
-    #New-VM -Location $folderLoc -Template $template -Name $VMname -VMHost $esx -Datastore $ds -DiskStorageFormat Thin | Set-VM -NumCpu 2 -MemoryGB 4 -Confirm:$false 
+    		#New-VM -Location $folderLoc -Template $template -Name $VMname -VMHost $esx -Datastore $ds -DiskStorageFormat Thin | Set-VM -NumCpu 2 -MemoryGB 4 -Confirm:$false 
 		New-VM -Location $folderLoc -Template $template -Name $VMname -ResourcePool $rp -Datastore $ds -DiskStorageFormat Thin | Set-VM -NumCpu 2 -MemoryGB 4 -Confirm:$false 
 		Sleep 10 #TO AVOID SUCCEEDING DISK I/O
 		}
 	$i++
 	}
   
-Write-Host "Completed!!!"	
+Write-Host "Completed!!!"
