672d54187d0eeeb08096ae414a03bf125adb0c55
Saddam ZEMMALI <saddam.zemmali@gmail.com>
2019-03-24 19:23:49 +0100

    Update vCenter-Login-Admin.ps1

--- vCenter-Login-Admin.ps1
+++ vCenter-Login-Admin.ps1
@@ -4,8 +4,9 @@ function vCenter-Login-Admin
 {
 $server = "VCENTER_FQDN"
 $user = "administrator@vsphere.local"
-$pass = "PASSWORD"
-Connect-VIServer -Server $server -User $user -Password $pass
+$password = Read-Host 'Enter The vCenter Password' -AsSecureString
+$vccredential = New-Object System.Management.Automation.PSCredential ($username, $password)
+Connect-VIServer -Server $server -Cred $vccredential -ErrorAction SilentlyContinue -WarningAction 0 | Out-Null
 }
 
 vCenter-Login-Admin
